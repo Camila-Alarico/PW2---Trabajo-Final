184686f0630910239a081ab99c58105353a77969
Camila Alarico <calarico@unsa.edu.pe>
2025-07-04 16:47:35 -0500

    Se agrega en base.html la opcion de navegar entre las paginas de la cunaunsa

--- cunaunsa/urls.py
+++ cunaunsa/urls.py
@@ -21,6 +21,6 @@ from admission.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', home, name='home'),
     path('', include('admission.urls')),
 ]

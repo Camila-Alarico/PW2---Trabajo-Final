7538ed2fc61585ce4916aac5e62ca3c26b850ffc
Cristhian <cristhian.bravo.8657@gmail.com>
2025-07-09 08:41:46 -0500

    Nuevas rutas en app.routes, se adapta el navbar.html. Aún no se han generado los componentes nuevos.

--- frontend/colegio-inicial/src/app/app.routes.ts
+++ frontend/colegio-inicial/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
+import { AboutUsComponent } from './pages/about-us/about-us';
+import { AdmissionsComponent } from './pages/admissions/admissions';
+import { GalleryComponent } from './pages/gallery/gallery';
+import { ContactPageComponent } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'admissions', component: AdmissionsComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'contact', component: ContactPageComponent },
+];

a3d910f5667ba7948005ba9250789ccea585d49d
Cristhian <cristhian.bravo.8657@gmail.com>
2025-07-13 14:07:56 -0500

    Ajustes para navbar fijo fuera de Home y ajuste de padding con clase page-content. Se actualizan algunas imagenes.

--- frontend/colegio-inicial/src/app/shared/navbar/navbar.ts
+++ frontend/colegio-inicial/src/app/shared/navbar/navbar.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { Component, OnInit, HostListener } from '@angular/core';
+
 @Component({
-  selector: 'app-navbar',
   standalone: true,
+  selector: 'app-navbar',
   imports: [RouterModule],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css']
 })
-export class NavbarComponent { }
+export class NavbarComponent implements OnInit {
+  isScrolled = false;
+
+  constructor(public router: Router) {}
+
+  get isHome(): boolean {
+    return this.router.url === '/home';
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 50;
+  }
+
+  ngOnInit(): void {
+    this.onWindowScroll(); 
+  }
+}
+

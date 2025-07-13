77392dfeebf480530919548b98d27ccd900aaa8c
Cristhian <cristhian.bravo.8657@gmail.com>
2025-07-13 12:42:31 -0500

    Implementación de rutas para secciones independientes y migración del landing a Home

--- frontend/colegio-inicial/src/app/app.routes.ts
+++ frontend/colegio-inicial/src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home';
-import { AboutUsComponent } from './pages/about-us/about-us';
-import { AdmissionsComponent } from './pages/admissions/admissions';
-import { GalleryComponent } from './pages/gallery/gallery';
-import { ContactPageComponent } from './pages/contact/contact';
+import { Home } from './pages/home/home';
+import { AboutUs } from './pages/about-us/about-us';
+import { Admissions } from './pages/admissions/admissions';
+import { Gallery } from './pages/gallery/gallery';
+import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'about-us', component: AboutUsComponent },
-  { path: 'admissions', component: AdmissionsComponent },
-  { path: 'gallery', component: GalleryComponent },
-  { path: 'contact', component: ContactPageComponent },
+  { path: 'home', component: Home },
+  { path: 'about-us', component: AboutUs },
+  { path: 'admissions', component: Admissions },
+  { path: 'gallery', component: Gallery },
+  { path: 'contact', component: Contact },
 ];
--- frontend/colegio-inicial/src/app/app.ts
+++ frontend/colegio-inicial/src/app/app.ts
@@ -1,28 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar';
-import { HeroComponent } from './home/hero/hero';
-import { ServicesComponent } from './home/services/services';
-import { AboutComponent } from './home/about/about';
-import { PortfolioComponent } from './home/portfolio/portfolio';
-import { TeamComponent } from './home/team/team';
 import { FooterComponent } from './shared/footer/footer';
 import { ContactComponent } from './home/contact/contact';
-//Se podria agregar contact component
+
 @Component({
   standalone: true,
-  //import RouterOutlet, fallaba
-  imports: [
-    NavbarComponent,
-    HeroComponent,
-    ServicesComponent,
-    AboutComponent,
-    PortfolioComponent,
-    TeamComponent,
-    ContactComponent,
-    FooterComponent
-  ],
+imports: [
+  RouterOutlet,
+  NavbarComponent,
+  FooterComponent
+],
   selector: 'app-root',
   templateUrl: './app.html',
 })
-export class App {}
+export class App { }

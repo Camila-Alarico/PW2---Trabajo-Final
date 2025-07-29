902685fd0b68a34d5eea5a1d8ca77f362e1d1a85
elkinramos8 <eramosoch@unsa.edu.pe>
2025-07-29 13:13:02 -0500

    implementado POST en /api/applicants/ para crear postulantes

--- admission/views.py
+++ admission/views.py
@@ -25,6 +25,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import status
 from django.shortcuts import render, redirect
 
 class ApplicantListViewAngular(ListAPIView):
@@ -137,6 +138,13 @@ class ApplicantListAPIView(APIView):
         applicants = Applicant.objects.all()
         serializer = ApplicantSerializer(applicants, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ApplicantSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # Poner base en la pagina principal
 def home(request):
